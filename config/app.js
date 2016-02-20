1ba684122bbda39aa7ffef940fc969001e914817
srinath <vjsrinath@live.com>
2016-02-20 21:10:12 +0530

    Added dockerfile to generate docker container continuously

--- config/app.js
+++ config/app.js
@@ -1,6 +1,6 @@
 module.exports = {
-  appName: 'OAuth2 Ludicrum API',
-  port: 1337,
+    appName: 'Ludicrum Users Services',
+    port: process.env.PORT || 1337,
   oauth: {
     tokenLife: 3600
   },
